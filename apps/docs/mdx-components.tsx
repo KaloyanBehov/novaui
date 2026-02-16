c18c1ce4066bcdd86a6b558440c19c78165c7777
KaloyanBehov <kaloqnbehov@gmail.com>
2026-02-16 20:24:35 +0200

    mdx docs

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,11 +1,14 @@
 // @ts-nocheck
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs'
 import { ComponentPreview } from '@/components/ui/component-preview'
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Tab,
+    Tabs,
     ComponentPreview,
     ...components,
   }
