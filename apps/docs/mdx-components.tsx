11346618eb2e62d81e1a1f824994603b5ed0de2e
KaloyanBehov <kaloqnbehov@gmail.com>
2026-02-16 20:11:19 +0200

    mdx button preview

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,10 +1,12 @@
 // @ts-nocheck
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
+import { ComponentPreview } from '@/components/ui/component-preview'
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ComponentPreview,
     ...components,
   }
 }
