8acb0315b29564a367ce9f16271f91df5d3b1f45
kbehov <kbehov@gmail.com>
2026-02-16 19:50:42 +0200

    remove component preview

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,15 +1,10 @@
 // @ts-nocheck
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
-import { ComponentPreview, ComponentDemo } from './components/component-preview'
-import { Button } from './components/mdx-button-wrapper'
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    ComponentPreview,
-    ComponentDemo,
-    Button,
     ...components,
   }
 }
